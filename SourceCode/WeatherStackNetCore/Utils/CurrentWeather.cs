1845418cce7a5439cdde323597c475a27559fe0e
Serdar GÜL <serdargul@outlook.com>
2022-07-08 12:48:14 +0300

    Error handling and showing have been done in current weather queries of API

--- SourceCode/WeatherStackNetCore/Utils/CurrentWeather.cs
+++ SourceCode/WeatherStackNetCore/Utils/CurrentWeather.cs
@@ -10,4 +10,6 @@ public class CurrentWeather
     public GeneralRequest request { get; set; }
     public Location location { get; set; }
     public Current current { get; set; }
+    public string success { get; set; }
+    public Error error { get; set; }
 }
