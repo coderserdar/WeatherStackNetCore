b31bb264900d040b61827f73083e1d545b61b650
Serdar GÜL <serdargul@outlook.com>
2022-07-08 09:42:15 +0300

    API Key Commit
    API Key has been taken from appSettings.json file for more security. Some comments have been added

--- SourceCode/WeatherStackNetCore/Controllers/CurrentWeatherController.cs
+++ SourceCode/WeatherStackNetCore/Controllers/CurrentWeatherController.cs
@@ -8,32 +8,60 @@ namespace WeatherStackNetCore.Controllers;
 
 public class CurrentWeatherController : Controller
 {
+    /// <summary>
+    /// Config element
+    /// </summary>
+    private IConfiguration configuration;
+
+    /// <summary>
+    /// This is the constructor of controller
+    /// </summary>
+    /// <param name="config"></param>
+    public CurrentWeatherController(IConfiguration config)
+    {
+        // this is used to get some key values from appSettings.Json
+        configuration = config;
+    }
+    
+    /// <summary>
+    /// This is used to show Current Weather search page
+    /// </summary>
+    /// <returns>Current Weather search page</returns>
     public IActionResult Index()
     {
         var model = new CurrentWeatherViewModel();
         return View(model);
     }
 
+    /// <summary>
+    /// This method is used to make WeaherStack Current API call
+    /// And bring the result to the screen
+    /// </summary>
+    /// <param name="query">General Parameter (Like City Name, County Name etc)</param>
+    /// <param name="unit">Unit Parameter</param>
+    /// <param name="language">Language Parameter (You shouldn't fill this parameter if your key is free version)</param>
+    /// <returns>Current Weather Info</returns>
     [HttpPost]
     public async Task<CurrentWeather?> GetCurrentWeather(string query, string unit, string language)
     {
-        var API_key = "7aae0d0b8034def58abb3a31f6e317b0";
+        var apiKey = configuration.GetValue<string>("API_Key"); 
         
-        var http = new HttpClient();
-        http.Timeout = new TimeSpan(0, 0, 30);
-        var requestString = "http://api.weatherstack.com/" + $"current?access_key={API_key}&query={query}";
+        var httpClient = new HttpClient();
+        httpClient.Timeout = new TimeSpan(0, 0, 30);
+        
+        var requestString = "http://api.weatherstack.com/" + $"current?access_key={apiKey}&query={query}";
         if (!string.IsNullOrEmpty(unit))
             requestString += $"&unit={unit}";
         if (!string.IsNullOrEmpty(language))
             requestString += $"&language={language}";
-        var response = await http.GetAsync(requestString);
+        
+        var response = await httpClient.GetAsync(requestString);
         var result = await response.Content.ReadAsStringAsync();
 
         var serializer = new DataContractJsonSerializer(typeof(CurrentWeather));
+        var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+        var currentWeather = (CurrentWeather)serializer.ReadObject(memoryStream)!;
 
-        var memorystream = new MemoryStream(Encoding.UTF8.GetBytes(result));
-        var data = (CurrentWeather)serializer.ReadObject(memorystream);
-
-        return data;
+        return currentWeather;
     }
 }
