1c57a281574e636b27cf1a17cc871d97d548f057
Serdar GÜL <serdargul@outlook.com>
2022-07-08 12:11:05 +0300

    some classes has been added

---
+++ SourceCode/WeatherStackNetCore/Utils/AutoComplete.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherStackNetCore.Utils;
+
+/// <summary>
+/// API Request Result Class
+/// </summary>
+public class AutoComplete
+{
+    public LocationRequest request { get; set; }
+    public List<Location> results { get; set; }
+}
