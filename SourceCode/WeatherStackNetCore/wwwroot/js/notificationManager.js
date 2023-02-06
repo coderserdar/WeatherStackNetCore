18740dba9b5547d825f1413a08355b9e5b3b3d27
Serdar GÜL <serdargul@outlook.com>
2023-02-06 13:58:23 +0300

    Lobibox message integration has been made by the pages which uses AJAX calls in jquery

---
+++ SourceCode/WeatherStackNetCore/wwwroot/js/notificationManager.js
@@ -0,0 +1,34 @@
+﻿//Look documentation for more detail http://lobianijs.com/site/lobibox#lobibox-notification-usage
+var MyBookShelfWeb = MyBookShelfWeb || {};
+
+
+MyBookShelfWeb.notification = {
+
+    ShowErrorMessage: function (title,message) {
+        Lobibox.notify('error', {
+            title:title,
+            msg: message
+        });
+    },
+    
+    ShowOKMessage: function (title, message) {
+        Lobibox.notify('success', {
+            title: title,
+            msg: message
+        });
+    },
+
+    ShowInfoMessage: function (title, message) {
+        Lobibox.notify('info', {
+            title: title,
+            msg: message
+        });
+    },
+
+    ShowWarningMessage: function (title, message) {
+        Lobibox.notify('warning', {
+            title: title,
+            msg: message
+        });
+    }
+}
